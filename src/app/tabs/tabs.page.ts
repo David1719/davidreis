92fa0c5055759ae26fec875666b9aaf7f3f6ddce
Técnico em Informática 2018.2 <39121742018.2@senacrjedu.local>
2019-10-14 11:31:40 -0300

    andamento

---
+++ src/app/tabs/tabs.page.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { PlayerService } from '../services/player.service';
+import { GameService } from '../services/game.service';
+
+@Component({
+  selector: 'app-tabs',
+  templateUrl: 'tabs.page.html',
+  styleUrls: ['tabs.page.scss']
+})
+export class TabsPage {
+
+  protected quantPlayer:number = 0;
+  protected quantGame:number = 0;
+
+  constructor(
+    protected playerService: PlayerService,
+    protected gameService: GameService,
+  ) {
+    this.playerService.gelAll().subscribe(
+      res=>{
+        this.quantPlayer = res.length
+      }
+    ),
+    this.gameService.gelAll().subscribe(
+      res=>{
+        this.quantGame = res.length
+      }
+    )
+
+  }
+
+}
